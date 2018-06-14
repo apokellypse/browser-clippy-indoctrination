51030a0901d69cd6c763b3e5aca568482bc955ef
Kelly Yu <kyu@yext.com>
2018-06-14 12:00:19 -0400

    clippy not defined

--- inject.js
+++ inject.js
@@ -2,6 +2,33 @@
 
 (function() {
 
+	function loadjscssfile(filename, filetype) {
+		if (filetype == "js"){ //if filename is a external JavaScript file
+			var fileref=document.createElement('script')
+			fileref.setAttribute("type","text/javascript")
+			fileref.setAttribute("src", filename)
+		}
+		else if (filetype == "css"){ //if filename is an external CSS file
+			var fileref=document.createElement("link")
+			fileref.setAttribute("rel", "stylesheet")
+			fileref.setAttribute("type", "text/css")
+			fileref.setAttribute("href", filename)
+		}
+		if (typeof fileref != "undefined")
+		document.getElementsByTagName("head")[0].appendChild(fileref)
+	}
+
+	var jqueryUrl = chrome.runtime.getURL("jquery-1.7.min.js");
+	var agentUrl = chrome.runtime.getURL("script.js");
+	var jsUrl = chrome.runtime.getURL("clippy.min.js");
+	var cssUrl = chrome.runtime.getURL("clippy.css");
+  	// document.getElementById("someImage").src = imgURL;
+
+  	loadjscssfile(jqueryUrl, "js");
+	loadjscssfile(jsUrl, "js"); //dynamically load and add this .js file
+	loadjscssfile(cssUrl, "css"); ////dynamically load and add this .css file
+	loadjscssfile(agentUrl, "js");
+
 	// just place a div at top right
 	var div = document.createElement('div');
 	div.style.position = 'fixed';
@@ -11,5 +38,12 @@
 	document.body.appendChild(div);
 
 	alert('inserted self... giggity');
+	console.log('hi');
+	console.log(clippy);
+
+	clippy.load('Clippy', function(agent){
+		// do anything with the loaded agent
+		agent.show();
+    });
 
 })();
---
+++ script.js
@@ -0,0 +1,6 @@
+clippy.load('Clippy', function(agent){
+	console.log('in agent');
+	// do anything with the loaded agent
+	agent.show();
+	console.log(agent);
+});
